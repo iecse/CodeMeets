e0778f93ec029ae49e469b7bc37a5c276be24177
Aakash Pahwa <aakash10399@gmail.com>
2018-08-14 12:38:42 +0530

    needs testing and cases, will do after classes today

--- heaps-java/maxHeap.java
+++ heaps-java/maxHeap.java
@@ -1,48 +1,106 @@
 import java.util.Scanner;
 class maxHeapObject
 {
-    public final int MAX_SIZE = 1000000;
+    public final int MAX_SIZE = 100;
     public int[] heap;
     public int heap_length;
-    maxHeapObject()
+    minHeapObject()
     {
         heap = new int[MAX_SIZE];
         heap_length = 0;
     }
-    public void heapifyUp(int index)
+    minHeapObject(int arr[])
+    {
+        heap = arr;
+        heap_length = heap.length;
+        for(int i=heap_length/2;i>0;i--)
+            heapify(i);
+    }
+    public void heapify(int index)
     {
         int l1 = 2*index;
         int l2 = (2*index) + 1;
-        int largest = index;
-        if(l1<=this.heap_length && this.heap[l1]>this.heap[largest])
-            largest = l1;
-        if(l2<=this.heap_length && this.heap[l2]>this.heap[largest])
-            largest = l2;
-        if(largest!=index)
+        int smallest = index;
+        if(l1<=this.heap_length && this.heap[l1]>this.heap[smallest])
+            smallest = l1;
+        if(l2<=this.heap_length && this.heap[l2]>this.heap[smallest])
+            smallest = l2;
+        if(smallest!=index)
         {
             int temp = this.heap[index];
-            this.heap[index] = this.heap[largest];
-            this.heap[largest] = temp;
-            if((index/2)>0)
-                this.heapifyUp(index/2);
+            this.heap[index] = this.heap[smallest];
+            this.heap[smallest] = temp;
+            this.heapify(smallest);
         }
     }
-    public void heapifyDown(int index)
+    public void insertKey(int x)
     {
-        int l1 = 2*index;
-        int l2 = (2*index) + 1;
-        int largest = index;
-        if(l1<=this.heap_length && this.heap[l1]>this.heap[largest])
-            largest = l1;
-        if(l2<=this.heap_length && this.heap[l2]>this.heap[largest])
-            largest = l2;
-        if(largest!=index)
+        if(heap_length==MAX_SIZE-1)
+            System.out.println("Overflow");
+        else
         {
-            int temp = this.heap[index];
-            this.heap[index] = this.heap[largest];
-            this.heap[largest] = temp;
-            this.heapifyDown(largest);
+            heap[++heap_length] = x;
+            int parent = heap_length/2,child = heap_length;
+            while (parent>0 && heap[parent]<heap[child])
+            {
+                int temp = heap[parent];
+                heap[parent] = heap[child];
+                heap[child] = temp;
+                child = parent;
+                parent/=2;
+            }
         }
+    }
+    public void increaseKey(int i,int new_val)
+    {
+        if(new_val<=heap[i])
+        {
+            System.out.println("Value Error");
+            return;
+        }
+        heap[i] = new_val;
+        while (i>=1 && (heap[i]<heap[i/2]))
+	    {
+            int temp = heap[i];
+            heap[i] = heap[i/2];
+            heap[i/2] = temp;
+		    i/=2;
+	    }
+    }
+    public void decreaseKey(int i,int new_val)
+    {
+        if (new_val>=heap[i])
+	    {
+		    System.out.println("Value Error");
+		    return;
+	    }
+	    heap[i] = new_val;
+	    heapify(i);
+    }
+    public void peek()
+    {
+        if(heap_length!=0)
+            System.out.println(heap[1]);
+        else
+            System.out.println("Underflow");
+    }
+    public void poll()
+    {
+        if(heap_length!=0)
+        {
+            if(heap_length>1)
+            {
+                int temp = heap[1];
+                heap[1] = heap[heap_length];
+                heap[heap_length] = temp;
+                System.out.println(heap[heap_length--]);
+                heapify(1);
+            }
+            else
+                System.out.println(heap[heap_length--]);
+        }
+        else
+            System.out.println("Underflow");
     }
 }
 public class maxHeap
@@ -50,43 +108,6 @@ public class maxHeap
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
-        int t = in.nextInt();
-        for(int i=0;i<t;i++)
-        {
-            maxHeapObject obj = new maxHeapObject();
-            int q = in.nextInt();
-            for(int j=0;j<q;j++)
-            {
-                int q1 = in.nextInt();
-                if(q1==1)
-                {
-                    obj.heap[++obj.heap_length] = in.nextInt();
-                    if(obj.heap_length>1)
-                        obj.heapifyUp(obj.heap_length/2);     
-                }
-                else if(q1==2)
-                {
-                    if(obj.heap_length==0)
-                    {
-                        System.out.println("Heap is empty!");
-                        continue;
-                    }
-                    System.out.println(obj.heap[1]);
-                }
-                else
-                {
-                    if(obj.heap_length==0)
-                    {
-                        System.out.println("Heap is empty!");
-                        continue;
-                    }
-                    int temp = obj.heap[obj.heap_length];
-                    obj.heap[obj.heap_length] = obj.heap[1];
-                    obj.heap[1] = temp;
-                    obj.heap_length--;
-                    obj.heapifyDown(1);
-                }
-            }
-        }
+        maxHeapObject obj = new maxHeapObject();
     }
 }
