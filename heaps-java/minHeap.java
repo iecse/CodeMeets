48962f057abe6c44068517a0d45986899115662d
Aakash Pahwa <aakash10399@gmail.com>
2018-08-14 12:39:12 +0530

    needs testing and cases, will do after classes today

--- heaps-java/minHeap.java
+++ heaps-java/minHeap.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 class minHeapObject
 {
-    public final int MAX_SIZE = 1000000;
+    public final int MAX_SIZE = 100;
     public int[] heap;
     public int heap_length;
     minHeapObject()
@@ -9,7 +9,14 @@ class minHeapObject
         heap = new int[MAX_SIZE];
         heap_length = 0;
     }
-    public void heapifyUp(int index)
+    minHeapObject(int arr[])
+    {
+        heap = arr;
+        heap_length = heap.length;
+        for(int i=heap_length/2;i>0;i--)
+            heapify(i);
+    }
+    public void heapify(int index)
     {
         int l1 = 2*index;
         int l2 = (2*index) + 1;
@@ -23,26 +30,77 @@ class minHeapObject
             int temp = this.heap[index];
             this.heap[index] = this.heap[smallest];
             this.heap[smallest] = temp;
-            if((index/2)>0)
-                this.heapifyUp(index/2);
+            this.heapify(smallest);
         }
     }
-    public void heapifyDown(int index)
+    public void insertKey(int x)
     {
-        int l1 = 2*index;
-        int l2 = (2*index) + 1;
-        int smallest = index;
-        if(l1<=this.heap_length && this.heap[l1]<this.heap[smallest])
-            smallest = l1;
-        if(l2<=this.heap_length && this.heap[l2]<this.heap[smallest])
-            smallest = l2;
-        if(smallest!=index)
+        if(heap_length==MAX_SIZE-1)
+            System.out.println("Overflow");
+        else
         {
-            int temp = this.heap[index];
-            this.heap[index] = this.heap[smallest];
-            this.heap[smallest] = temp;
-            this.heapifyDown(smallest);
+            heap[++heap_length] = x;
+            int parent = heap_length/2,child = heap_length;
+            while (parent>0 && heap[parent]>heap[child])
+            {
+                int temp = heap[parent];
+                heap[parent] = heap[child];
+                heap[child] = temp;
+                child = parent;
+                parent/=2;
+            }
         }
+    }
+    public void increaseKey(int i,int new_val)
+    {
+        if(new_val<=heap[i])
+        {
+            System.out.println("Value Error");
+            return;
+        }
+        heap[i] = new_val;
+        while (i>=1 && (heap[i]>heap[i/2]))
+	    {
+            int temp = heap[i];
+            heap[i] = heap[i/2];
+            heap[i/2] = temp;
+		    i/=2;
+	    }
+    }
+    public void decreaseKey(int i,int new_val)
+    {
+        if (new_val>=heap[i])
+	    {
+		    System.out.println("Value Error");
+		    return;
+	    }
+	    heap[i] = new_val;
+	    heapify(i);
+    }
+    public void peek()
+    {
+        if(heap_length!=0)
+            System.out.println(heap[1]);
+        else
+            System.out.println("Underflow");
+    }
+    public void poll()
+    {
+        if(heap_length!=0)
+        {
+            if(heap_length>1)
+            {
+                int temp = heap[1];
+                heap[1] = heap[heap_length];
+                heap[heap_length] = temp;
+                System.out.println(heap[heap_length--]);
+                heapify(1);
+            }
+            else
+                System.out.println(heap[heap_length--]);
+        }
+        else
+            System.out.println("Underflow");
     }
 }
 public class minHeap
@@ -51,38 +109,5 @@ public class minHeap
     {
         Scanner in = new Scanner(System.in);
         minHeapObject obj = new minHeapObject();
-        int q = in.nextInt();
-        for(int j=0;j<q;j++)
-        {
-            int q1 = in.nextInt();
-            if(q1==1)
-            {
-                obj.heap[++obj.heap_length] = in.nextInt();
-                if(obj.heap_length>1)
-                    obj.heapifyUp(obj.heap_length/2);     
-            }
-            else if(q1==2)
-            {
-                if(obj.heap_length==0)
-                {
-                    System.out.println("Heap is empty!");
-                    continue;
-                }
-                System.out.println(obj.heap[1]);
-            }
-            else
-            {
-                if(obj.heap_length==0)
-                {
-                    System.out.println("Heap is empty!");
-                    continue;
-                }
-                int temp = obj.heap[obj.heap_length];
-                obj.heap[obj.heap_length] = obj.heap[1];
-                obj.heap[1] = temp;
-                obj.heap_length--;
-                obj.heapifyDown(1);
-            }
-        }
     }
 }
