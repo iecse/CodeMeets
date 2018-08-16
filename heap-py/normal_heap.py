677502e09e4dd773c55973ce187b3782a28fc06b
Aakash Pahwa <aakash10399@gmail.com>
2018-08-16 23:01:11 +0530

    fixed an indexing error in range() func

--- heap-py/normal_heap.py
+++ heap-py/normal_heap.py
@@ -3,40 +3,32 @@ def heapify(a,i):
 	right_child	= 2*i+1 
 	left_child = 2*i 
 	index=i
-	if a[i]<right_child and right_child<len(a):
-		temp=a[i]                                              """swaping the larger element to the parent node"""
-		a[i]=a[right_child]
-		a[right_child]=temp
+	if right_child<len(a) and a[index]<a[right_child]:
 		index=right_child
-
-	if a[i]<left_child and left_child<len(a):
-		temp=a[i]                                              """swaping the larger element to the parent node"""
-		a[i]=a[left_child]
-		a[left_child]=temp
+	if left_child<len(a) and a[index]<a[left_child]:
 		index=left_child
-
-	if index==i:
-		return
-
-	
-	heapify(a,index)
+	if index!=i:
+		temp = a[i]
+		a[i] = a[index]
+		a[index] = temp
+		heapify(a,index)
 
 
 def push_heap(a,val):
 	a.append(val)
 	i=len(a)-1
 	while i!=0 and a[i]>a[i/2]:
-		temp=a[i]                                              """swaping the larger element to the parent node"""
+		temp=a[i]                                              #swaping the larger element to the parent node
 		a[i]=a[i/2]
 		a[i/2]=temp
 		i=i/2
 
 
 def pop_heap(a):
-	if(len(a)==0)
+	if(len(a)==0):
 		print("not possible heap is empty")
 		return
-	if(len(a)==1)
+	if(len(a)==1):
 		a.pop(1)
 
 	a[1]=a[len(a)-1]
@@ -49,7 +41,7 @@ def increase_key(a,i,val):
 		print("value entered is lower or equal than existing value use some other function")
 		return
 	
-	while i>=1 and a[i]>a[i/2]:
+	while i>=2 and a[i]>a[i/2]:
 		temp=a[i]
 		a[i]=a[i/2]
 		a[i/2]=temp
@@ -63,8 +55,11 @@ def decrease_key(a,i,val):
 	heapify(a,i)
 
 def buildheap(a):
-	for i in range(len(a)/2,1,-1):
+	for i in range(len(a)//2,0,-1):
 		heapify(a,i)
 
 def maxelem(a):
 	print("max element is",a[1])
+heap = [1,2,0,4,4,-1,4,55,-99]
+buildheap(heap)
+maxelem(heap)
