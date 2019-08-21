b4dba7037eef5edb06cb3684a46c7f98819b98c7
Sourav Agrawal <souravagrawal.1023@gmail.com>
2019-08-22 04:07:13 +0530

    Java Solutions added

---
+++ graphs/Questions/Playing_With_Numbers/Java/PLYNUM.java
@@ -0,0 +1,52 @@
+import java.util.*;
+class PLYNUM{
+
+	public static int bfs(int a,int b){
+
+		int v1,v2,v3;
+		HashMap<Integer,Integer> hm=new HashMap<Integer,Integer>();
+		Queue<Integer> q=new LinkedList<Integer>();
+		q.add(a);
+		hm.put(a,0);
+
+		while(!q.isEmpty()){
+			int u=q.poll();
+			int dist=hm.get(u);
+			if(u==b)
+				return dist;
+				
+			v1=u-1;
+			v2=u+3;
+			v3=u*2;
+
+			if(v1>=0 && v1<=4000 && !hm.containsKey(v1)){
+				hm.put(v1,dist+1);
+				q.add(v1);
+			}
+			if(v2>=0 && v2<=4000 && !hm.containsKey(v2)){
+				hm.put(v2,dist+1);
+				q.add(v2);
+			}
+			if(v3>=0 && v3<=4000 && !hm.containsKey(v3)){
+				hm.put(v3,dist+1);
+				q.add(v3);
+			}
+		}
+		return -1;
+	}
+
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		int t,a,b,ans;
+		t=sc.nextInt();
+		while(t-->0){
+			a=sc.nextInt();
+			b=sc.nextInt();
+			if(b<=a)
+				ans=a-b;
+			else 
+				ans=bfs(a,b);
+			System.out.println(ans);
+		}
+	}
+}
---
+++ graphs/Questions/Social_Network/Java/SNET.java
@@ -0,0 +1,77 @@
+import java.util.*;
+class SNET{
+
+	public static int dfs(HashMap<String,ArrayList<String>> adj, HashMap<String,Boolean> vis,String curr,int count){
+		vis.put(curr,true);
+		count++;
+		for(String v: adj.get(curr)){
+			if(!vis.get(v))
+				count=dfs(adj,vis,v,count);
+		}
+		return count;
+	}
+
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		String x,a,b;
+		HashMap<String,ArrayList<String>> adj=new HashMap<String,ArrayList<String>>();
+		HashMap<String,Boolean> vis=new HashMap<String,Boolean>();
+		int k;
+		x=sc.next();
+		k=sc.nextInt();
+		while(k-->0){
+			a=sc.next();
+			b=sc.next();
+			ArrayList<String> u;
+			ArrayList<String> v;
+
+			if(!adj.containsKey(a) && !adj.containsKey(b)){
+				u=new ArrayList<String>();
+				v=new ArrayList<String>();
+				u.add(b);
+				v.add(a);
+			}
+
+			else if(!adj.containsKey(a)){
+				u=new ArrayList<String>();
+				v=adj.get(b);
+				u.add(b);
+				v.add(a);
+			}
+			else if(!adj.containsKey(b)){
+				u=adj.get(a);
+				v=new ArrayList<String>();
+				u.add(b);
+				v.add(a);
+			}
+			else{
+				u=adj.get(a);
+				v=adj.get(b);
+				u.add(b);
+				v.add(a);
+			}
+			adj.put(a,u);
+			adj.put(b,v);
+			vis.put(a,false);
+			vis.put(b,false);
+		}
+		vis.put(x,true);
+		int max=0;
+		String ans="";
+		Collections.sort(adj.get(x));
+		for(String u: adj.get(x)){
+			int temp;
+			if(!vis.get(u)){
+				temp= dfs(adj,vis,u,0);
+				if(temp>max){
+					max=temp;
+					ans=u;
+				}
+			}
+		}
+		System.out.println(ans);
+		System.out.println(max);
+
+
+	}
+}
