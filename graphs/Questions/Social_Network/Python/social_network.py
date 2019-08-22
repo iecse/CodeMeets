950c33b9787986f4f2521997ae231b51c0d30265
Sanchit Sahay <sanchitsahay.tech@gmail.com>
2019-08-22 09:27:03 +0530

    Added Python Solutions

---
+++ graphs/Questions/Social_Network/Python/social_network.py
@@ -0,0 +1,53 @@
+from collections import defaultdict
+
+adj = defaultdict(list)
+visited = defaultdict(bool)
+
+ans = 0
+
+def dfs(u):
+    stack = []
+    stack.append(u)
+    count = 0
+
+    while len(stack):
+        s = stack.pop()
+
+        if not visited[s]:
+            count += 1
+            visited[s] = True
+
+        for i in adj[s]:
+            if not visited[i]:
+                stack.append(i)
+
+    return count
+        
+
+    
+s = input().strip()
+
+n = int(input().strip())
+
+for i in range(n):
+    u,v = input().strip().split(" ")
+    adj[u].append(v)
+    adj[v].append(u)
+
+adj[s].sort()
+
+maxi = 0
+ch = "A"
+
+visited[s] = True
+
+for i in adj[s]:
+    if not visited[i]:
+        ans = dfs(i)
+        if ans > maxi:
+            maxi = ans
+            ch = i
+
+
+print(ch)
+print(maxi)
